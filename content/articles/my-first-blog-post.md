f7f18f9bdf36f77fc9239e03096ad3c1af51ddee
songselasak <song.selasak@gmail.com>
2021-10-19 11:52:59 +0700

    [TASK] Continue the work * Add Toc, HTML in md, Vue components, and Author

--- content/articles/my-first-blog-post.md
+++ content/articles/my-first-blog-post.md
@@ -3,9 +3,42 @@ title: My first Blog Post
 description: Learning how to use @nuxt/content to create a blog
 img: first-blog-post.jpg
 alt: my first blog post
+
+author:
+  name: Selasak
+  bio: All about Selasak
+  image: https://images.unsplash.com/photo-1593643946890-b5b85ade6451?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1978&q=80
+
 ---
 
+<!-- <author :author="author"></author> -->
+
+<div class="p-4 mb-4 text-white bg-blue-500">
+  This is HTML inside markdown that has a class of note
+</div>
+
+<info-box>
+  <template #info-box>
+    This is a vue component inside markdown using slots
+  </template>
+</info-box>
 
 # My first blog post
 
 Welcome to my first blog post using content module
+
+## This is a heading
+
+This is some more info
+
+### This is a sub heading
+
+This is some more info
+
+### This is another sub heading
+
+This is some more info
+
+## This is another heading
+
+This is some more info
--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,13 +1,23 @@
 <template>
   <article>
+      <nav>
+        <ul>
+            <li v-for="link of article.toc" :key="link.id">
+            <NuxtLink :class="{ 'py-2': link.depth === 2, 'ml-2 pb-2': link.depth === 3 }" :to="`#${link.id}`">{{ link.text }}</NuxtLink>
+            </li>
+        </ul>
+       </nav>
     <h1>{{ article.title }}</h1>
     <p>{{ article.description }}</p>
     <img :src="require(`~/assets/images/${article.img}`)" :alt="article.alt" />
     <p>Article last updated: {{ formatDate(article.updatedAt) }}</p>
+    <!-- <pre>{{ article }}</pre> -->
 
     <nuxt-content :document="article" />
+
+    <author :author="article.author" />
   </article>
-</template>l9
+</template>
 
 <script>
   export default {
@@ -26,6 +36,27 @@
   }
   
 </script>
+
+<style>
+  .nuxt-content h2 {
+    font-weight: bold;
+    font-size: 28px;
+  }
+  .nuxt-content h3 {
+    font-weight: bold;
+    font-size: 22px;
+  }
+  .nuxt-content p {
+    margin-bottom: 20px;
+  }
+  /* .icon.icon-link {
+    background-image: url('~assets/svg/icon-hashtag.svg');
+    display: inline-block;
+    width: 20px;
+    height: 20px;
+    background-size: 20px 20px;
+  } */
+</style>
 
 
 
