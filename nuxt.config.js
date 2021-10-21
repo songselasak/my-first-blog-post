6b04c630d4b2f183c6a748ef1221b479e53b6eef
songselasak <song.selasak@gmail.com>
2021-10-21 19:56:52 +0700

    [TASK] Added Ant-design

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,10 +18,12 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    "~/assets/variables.less"
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    ["@/plugins/antd-ui"]
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -50,5 +52,12 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    loaders: {
+      less: {
+        lessOptions: {
+          javascriptEnabled: true,
+        },
+     },
+   },
   }
 }
